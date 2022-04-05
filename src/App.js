fd865600c8ed0c04777cc626afc026c19e011ead
Blake Elliott <blakeelliott@Blakes-MacBook-Pro.local>
2022-04-05 12:25:50 -0500

    start of 2.4

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ import Gallery from "./components/Gallery";
 function App() {
   const [categories] = useState([
     {
-        name: 'commercial',
-        description: 'Photos of grocery stores, food trucks, and other commercial projects',
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-]);
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav categories={categories}
+      <Nav
+        categories={categories}
         setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory} />
+        currentCategory={currentCategory}
+      />
       <main>
-      <Gallery></Gallery>
+        <Gallery  currentCategory={currentCategory}/>
+
+        
         <About />
       </main>
     </div>
