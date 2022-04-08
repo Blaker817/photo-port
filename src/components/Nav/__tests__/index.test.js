c489fcca2b0b35be84f57c39ea2aab7c4115ad2a
Blake Elliott <blakeelliott@Blakes-MacBook-Pro.local>
2022-04-07 19:32:00 -0500

    run test for 20.4.4

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -7,6 +7,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 afterEach(cleanup);
 
 describe('Nav component', () => {
@@ -14,7 +16,10 @@ describe('Nav component', () => {
     it('renders', () => {
         render(<Nav categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
-            currentCategory={mockCurrentCategory} />);
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+        />);
     });
 
     // snapshot test
